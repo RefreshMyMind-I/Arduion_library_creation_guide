4ad9aa0e6dca5ec6a44dac667bb7d7982799e2d3
Kirtiraj <refreshmymind0@gmail.com>
2023-02-14 09:03:24 +0530

    added other functions in main code

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,19 @@ void setup() {
 
   Serial.begin(9600); // setting baud rate to make use of serial monitor in aruino.
   add = testlib.sum(1,2); // calling library function or the name you declared in define section.
-  Serial.print(add); // printing sum of to int to the serial monitor. 
+  Serial.print("Addition is: ");
+  Serial.println(add); // printing sum of to int to the serial monitor. 
+  int sub = testlib.sub(5,4);
+  Serial.print("Substraction is: ");
+  Serial.println(sub);
+  int mul = testlib.mul(2,2);
+  Serial.print("Multiplication is: ");
+  Serial.println(mul);
+  int div = testlib.div(20,5);
+  Serial.print("Divsion is: ");
+  Serial.print(div);
+  
+
 }
 
 void loop() {
